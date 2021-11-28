8c37c8c0f4d4e52ae40659ee7aa34759de61da71
mcquinca <89641085+mcquinnca@users.noreply.github.com>
2021-11-28 17:41:08 +0000

    Initial commit

---
+++ Question.java
@@ -0,0 +1,28 @@
+/**
+@author 
+@version 
+*/
+import java.util.ArrayList;
+import java.io.*;
+class Question{ 
+  ArrayList<String> Answers = new ArrayList<String>();
+  int Answer;
+
+/**
+This constructor method will construct a Qustion by reading the lines of the text file which relate to the number given
+@param aQuestionNumber the question number which you wish to create from the text file
+*/
+  void Question(int aQuestionNumber){
+    int qNumber = aQuestionNumber;
+    String question;
+    String answer1;
+    String answer2;
+    String answer3;
+    String answer4;
+    int correctAnswer;
+
+    try{
+    }
+
+  }
+}
